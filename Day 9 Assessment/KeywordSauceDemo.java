aa3561c91b68e9352b3c6806001e22412c058495
Sinchana sajjed <168188399+sinchana-sajjaid@users.noreply.github.com>
2026-09-16 23:05:41 +0530

    KeywordSauceDemo.java

---
+++ Day 9 Assessment/KeywordSauceDemo.java
@@ -0,0 +1,17 @@
+package KeywordDrivenFramework;
+
+import KeywordDriven.KeywordExecutorSauceDemo;
+
+public class KeywordSauceDemo {
+
+	public static void main(String[] args) {
+		KeywordExecutorSauceDemo e = new KeywordExecutorSauceDemo();
+		e.executor("OPEN_BROWSER");
+	    e.executor("OPEN_URL");
+	    e.executor("USERNAME");
+	    e.executor("PASSWORD");
+	    e.executor("LOGIN");
+	    e.executor("CLOSE_BROWSER");
+	}
+
+}
